acedb5693ec26cd52115157321043501f27042b6
Victor <vfairon@gmail.com>
2020-11-04 17:07:28 +0100

    table hidden + videos

--- js/java.js
+++ js/java.js
@@ -1,3 +1,79 @@
 $('.carousel').carousel({
     interval: 200
   })
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function second() {
+  var x = document.getElementById("second");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function third() {
+  var x = document.getElementById("third");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function four() {
+  var x = document.getElementById("four");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function five() {
+  var x = document.getElementById("five");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function six() {
+  var x = document.getElementById("six");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function seven() {
+  var x = document.getElementById("seven");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function eight() {
+  var x = document.getElementById("eight");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+function nine() {
+  var x = document.getElementById("nine");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    }
+} 
+
+
