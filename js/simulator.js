788bdcf91d8ce32f6c4d310b9d8f6322b357497e
Salaheddine EL BOUHLALI <el.salaheddine@hotmail.com>
2020-11-23 02:25:35 +0100

    MAJ 1.0  du simulator

--- js/simulator.js
+++ js/simulator.js
@@ -5,23 +5,20 @@ var i3 = document.getElementById("i3");
 var o1 = document.getElementById("o1");
 var o2 = document.getElementById("o2");
 var o3 = document.getElementById("o3");
-var led1 = document.getElementById("led1")
-var led2 = document.getElementById("led2")
-var led3 = document.getElementById("led3")
-var led4 = document.getElementById("led4")
-var led5 = document.getElementById("led5")
-var led6 = document.getElementById("led6")
+var led1 = document.getElementById("l1")
+var led2 = document.getElementById("l2")
+var led3 = document.getElementById("l3")
 var btnMoov = document.getElementById("movementBtn");
-var mvsensor = document.getElementById("mvsensor")
+var mvsensor = document.getElementById("i1")
 var x = 0;
-var clickedElement = [];
+var inOutPuts = [i2,i3,o2,o3];
 var slider = document.getElementById("myRange");
 var slider2 = document.getElementById("myRange2");
 var slider3  = document.getElementById("myRange3");
 var count = 0
 var ledOnPath = "../res/img/ledOn.png";
 var ledOffPath = "../res/img/led.png";
-var arrLeds  = [led1,led2,led3,led4,led5,led6]
+var arrLeds  = [led1,led2,led3]
 var i1OptionValue = document.getElementById("i1OptionValue");
 var i2OptionValue = document.getElementById("i2OptionValue");
 var i3OptionValue = document.getElementById("i3OptionValue");
@@ -38,10 +35,11 @@ var i3Description = document.getElementById("i3Description");
 var mvDescription = document.getElementById("mvDescription");
 var lightDescription = document.getElementById("lightDescription");
 var tempDescription = document.getElementById("tempDescription");
+var isHidden = true;
 
+var options = Array.from($(".optionss"))
 
-
-
-
-
-
+  inOutPuts.forEach(element=>{
+     console.log(element)
+ element.setAttribute("connected","false")
+ })
--- js/utils.js
+++ js/utils.js
@@ -1,66 +1,160 @@
 let i = 0;
-
 function clicked(event) {
     if ((i > 1 || i == 0) && $(event.target).parents(".output").length) return;
     if (i > 0 && $(event.target).parents(".input").length)
-
     {
         if (i == 1) return;
     }
     i++;
-    if (i > 2) {
-
-        document.querySelectorAll(".bg-light").forEach(element => {
-            element.classList.remove("bg-light")
+    if (i > 2) {      
+        document.querySelectorAll(".d-inline").forEach(element => {
+            element.classList.remove("d-inline")
+          
+           
+            
         });
+        let x = Array.from($("[connected=true]"))
+        x.forEach(element=>{
+                 element.setAttribute("connected","false")
+             }) 
         i = 1;
     }
+    var id = event.target.getAttribute("id");
+    let c = document.getElementById("u"+event.target.getAttribute("id"))
+    if(id[0]=="l")
+    {
+        c = document.getElementById("ul");
+        led1.setAttribute("connected","true")
+        led2.setAttribute("connected","true")
+        led3.setAttribute("connected","true")
 
-    event.target.classList.add("bg-light");
-
+    }
+    else{
+    
+        event.target.setAttribute("connected","true")
+    }
+    c.classList.add("d-inline")    
 }
-
 function power()
 {
-    if(slider3.value<50)
+    if(i3.getAttribute("connected")==="true"&& l1.getAttribute("connected")==="true")
     {
-        o3.setAttribute("src","../res/img/turn-off.png")
+        slide1(slider3)
     }
-    else
+    if(i3.getAttribute("connected")==="true"&& o3.getAttribute("connected")==="true")
     {
-        o3.setAttribute("src","../res/img/turn-on.png")
+
+        if(slider3.value<50)
+        {
+            o3.setAttribute("src","../res/img/turn-off.png")
+        }
+        else
+        {
+            o3.setAttribute("src","../res/img/turn-on.png")
+        }
+    }
+    if(i3.getAttribute("connected")==="true"&& o2.getAttribute("connected")==="true")
+    {
+        if (count < 30)
+        {
+            x = x + 6;
+            o2.style.transform = "rotate(" + x + "deg)"
+        }
+        else if (slider3.value > 30 && slider3.value < 60)
+        {
+            x = x + 10;
+            o2.style.transform = "rotate(" + x + "deg)"
+    
+        }
+        else if (slider3.value > 60 && slider3.value < 90)
+        {
+            x =x+12
+            o2.style.transform = "rotate(" + x + "deg)"
+    
+        }
+        else if(slider3.value > 90)
+            {
+                x=x+16
+                o2.style.transform = "rotate(" + x + "deg)"
+            }
     }
 }
-
 function strip()
 {
-    count++;
-    switch(count)
+   $("#i1").hover(function(){
+       $(this).css("background-color","black")
+       mvsensor.setAttribute("src","../res/img/sensorWhite.png")
+    },
+       function(){
+           setTimeout(3000)
+        $(this).css("background-color", "white")
+        mvsensor.setAttribute("src","../res/img/sensor.png")
+       }
+   )
+    if(mvsensor.getAttribute("connected")==="true"&& l1.getAttribute("connected")==="true")
     {
-        case 1:{
-            led1.setAttribute("src",ledOnPath);
-            break;
-        }
-        case 2:{
-            led2.setAttribute("src",ledOnPath);
-            break;
-        }
-        case 3:{
-            led3.setAttribute("src",ledOnPath);
-            break;
-        }
-        default :{
-            for(var i = 0;i<arrLeds.length;i++)
-            {
-                arrLeds[i].setAttribute("src",ledOffPath);
+        count++;
+        switch(count)
+        {
+            case 1:{
+                led1.setAttribute("src",ledOnPath);
+                break;
             }
-            count=0;
+            case 2:{
+                led2.setAttribute("src",ledOnPath);
+                break;
+            }
+            case 3:{
+                led3.setAttribute("src",ledOnPath);
+                break;
+            }
+            default :{
+                for(var i = 0;i<arrLeds.length;i++)
+                {
+                    arrLeds[i].setAttribute("src",ledOffPath);
+                }
+                count=0;
+            }
         }
-    }
-    
-}
 
-function slide1() {
+    }
+    if(mvsensor.getAttribute("connected")==="true"&& o2.getAttribute("connected")==="true")
+    {
+        count++
+        if(count == 1)
+        {
+            o2.style.transform = "rotate(" + 15 + "deg)"
+        }
+      else if(count == 2)
+      {
+        o2.style.transform = "rotate(" + 25 + "deg)"
+      }
+      else if(count == 3)
+      {
+        o2.style.transform = "rotate(" + 35 + "deg)"
+      }
+      else if(count == 4)
+      {
+        o2.style.transform = "rotate(" + 95 + "deg)"
+        count=0
+      }
+    
+    }
+    if(mvsensor.getAttribute("connected")==="true"&& o3.getAttribute("connected")==="true")
+      {     
+          var turn = "../res/img/turn-on.png";
+          var turnOff = "../res/img/turn-off.png";
+          if(o3.getAttribute("src")===turn) 
+          {
+              o3.setAttribute("src",turnOff)
+             
+          }
+          else{
+            o3.setAttribute("src",turn)
+          }
+      }   
+}
+function slide1(slider) {
 
     if (slider.value > 33) {
         led1.setAttribute("src", "../res/img/ledOn.png")
@@ -76,38 +170,51 @@ function slide1() {
         led3.setAttribute("src", "../res/img/ledOn.png")
     } else {
         led3.setAttribute("src", "../res/img/led.png")
-    }
-    
-
+    }    
 }
-
 function slide2() {
-
-
-
-    if (slider2.value < 30)
+    if(i2.getAttribute("connected")==="true"&& l1.getAttribute("connected")==="true")
     {
-
-        x = x + 6;
-        o2.style.transform = "rotate(" + x + "deg)"
+        slide1(slider2)
     }
-    else if (slider2.value > 30 && slider2.value < 60)
+    if(i2.getAttribute("connected")==="true"&& o3.getAttribute("connected")==="true")
     {
-        x = x + 10;
-        o2.style.transform = "rotate(" + x + "deg)"
-
-    }
-    else if (slider2.value > 60 && slider2.value < 90)
-    {
-        x =x+12
-        o2.style.transform = "rotate(" + x + "deg)"
-
-    }
-    else if(slider2.value > 90)
+        if(slider2.value <50)
         {
-            x=x+16
+            o3.setAttribute("src","../res/img/turn-off.png")
+        }
+        else{
+            o3.setAttribute("src","../res/img/turn-on.png")
+        }
+        return
+    }
+    if(i2.getAttribute("connected")==="true"&& o2.getAttribute("connected")==="true")
+    {
+
+        if (slider2.value < 30)
+        {
+    
+            x = x + 6;
             o2.style.transform = "rotate(" + x + "deg)"
         }
+        else if (slider2.value > 30 && slider2.value < 60)
+        {
+            x = x + 10;
+            o2.style.transform = "rotate(" + x + "deg)"
+    
+        }
+        else if (slider2.value > 60 && slider2.value < 90)
+        {
+            x =x+12
+            o2.style.transform = "rotate(" + x + "deg)"
+    
+        }
+        else if(slider2.value > 90)
+            {
+                x=x+16
+                o2.style.transform = "rotate(" + x + "deg)"
+            }
+    }
     var path = "../res/img/lightbulb.png"
     var pathOn = "../res/img/lightbulbon.png"
 
@@ -117,19 +224,23 @@ function slide2() {
     } else {
 
       i2.setAttribute("src", pathOn)
-    }
-    
-
-
+    }   
 }
-
-
 mvsensor.addEventListener("mouseenter",strip)
-
 function write1()
 {
-    i1OptionValue.style.display="inline"
-    i1OptionValue.style.width="100%"
+    if(i1OptionInput.value ==="")
+    {
+        i1OptionValue.style.visibility="hidden"
+    }
+    else{
+        i1OptionValue.style.visibility="visible"
+        i1OptionValue.style.display="inline"
+        i1OptionValue.style.width="100%"
+    }
+    
+    
+
    // console.log(i1OptionValue)
    i1OptionValue.innerHTML= i1OptionInput.value
    i1OptionInput.value =""
@@ -138,21 +249,34 @@ function write1()
 
 function write2()
 {
-    i2OptionValue.style.display="inline"
-    i2OptionValue.style.width="100%"
+    if(i2OptionInput.value ==="")
+    {
+        i2OptionValue.style.visibility="hidden"
+    }
+    else{
+
+        i2OptionValue.style.visibility="visible"
+        i2OptionValue.style.display="inline"
+        i2OptionValue.style.width="100%"
+    }
    // console.log(i1OptionValue)
    i2OptionValue.innerHTML= i2OptionInput.value
    i2OptionInput.value =""
 }
 function write3()
 {
-    i3OptionValue.style.display="inline"
-    i3OptionValue.style.width="100%"
+    if(i3OptionInput.value ==="")
+    {
+        i3OptionValue.style.visibility="hidden"
+    }
+    else{
+        i3OptionValue.style.visibility="visible"
+        i3OptionValue.style.display="inline"
+        i3OptionValue.style.width="100%"
+    }
    i3OptionValue.innerHTML= i3OptionInput.value
    i3OptionInput.value =""
 }
-
-
  function writeDescription1()
  {
      
@@ -183,6 +307,11 @@ function write3()
         }
    
  }
-
-
+ function hiddeOptions()
+ {
+            options.forEach(element=>{
+            element.classList.toggle("invisible")
+          })
+ 
+ }
 mvsensor.addEventListener("mouseenter",strip)
